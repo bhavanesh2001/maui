75dffe0132399ac1df5f6f740ef9d300559db735
Bhavanesh N <bhavanesh001@gmail.com>
2025-06-03 20:45:39 +0530

    setup stuff in sandbox

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,38 @@
-﻿namespace Maui.Controls.Sample;
+﻿using System.Collections.ObjectModel;
+
+namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
+	private ObservableCollection<string>? items;
+	public ObservableCollection<string>? Items
+	{
+		get => items;
+		set
+		{
+			items = value;
+			OnPropertyChanged();
+		}
+	}
+
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
+	}
+	
+	private void Button_Clicked2(object sender, EventArgs e)
+	{
+		Items = new ObservableCollection<string>();
+		for (int i = 1; i <= 40; i++)
+		{
+			Items.Add($"Item {i}");
+		}
+		MyCollectionView.SelectedItem = "Item 39";
+		Items = new ObservableCollection<string>();
+		for (int i = 1; i <= 10; i++)
+		{
+			Items.Add($"Item {i}");
+		}
 	}
 }
